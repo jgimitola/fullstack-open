e9a3b9ee0c5f0a72d5cbb3ce6aa5959b7895e8c0
Jesús Imitola <47477274+jgimitola@users.noreply.github.com>
2021-10-15 15:05:38 -0500

    feat: add morgan logger

--- part3/phonebook-backend/README.md
+++ part3/phonebook-backend/README.md
@@ -3,3 +3,4 @@
 Time of each session
 
 1. 83 mins
+2. 19 mins
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
+
 const app = express();
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 data = [
   {
